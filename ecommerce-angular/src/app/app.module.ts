bfa12c73023e4888f38b022055017ddeddcafe8e
Paul Pessoa <paulmspessoa@gmail.com>
2022-01-30 00:42:04 -0300

    primeira parte footer

--- ecommerce-angular/src/app/app.module.ts
+++ ecommerce-angular/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './components/template/header/header.component';
-import { FooterComponent } from './components/templates/footer/footer.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 
 @NgModule({
