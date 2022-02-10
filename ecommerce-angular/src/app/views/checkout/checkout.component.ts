636f49c30704e08bd190e1cf82b3a4cbac29e6f0
Paul Pessoa <paulmspessoa@gmail.com>
2022-02-09 23:17:07 -0300

    adicionar remover do carrinho

--- ecommerce-angular/src/app/views/checkout/checkout.component.ts
+++ ecommerce-angular/src/app/views/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from './checkout.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  listSelectedFilms: Film[] = [];
+  totalPrice|: number;
+  disabled = false;
+  hide = true;
+  form: any;
 
-  constructor() { }
+  constructor(private checkoutService: CheckoutService) { }
 
   ngOnInit(): void {
+    this.totalPrice = this.checkoutService.totalPrice;
+    this.listSelectedFilms = this.checkoutService.listSelectedFilms;
+    this.toggleButton();
   }
-
+  toggleButton() {
+    if (this.listSelectedFilms.length == 0) {
+      this.disabled = true;
+    }
+  }
 }
